f7644d9327ab62e0643992cc0e2db5b089b9a6d2
j0sh05 <jalix8@yahoo.com>
2022-06-21 19:38:20 -1000

    Initial Commit

---
+++ MyFinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFinalProject
+//
+//  Created by Joshua Krueger on 6/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFinalProject/MyFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinalProjectApp.swift
+//  MyFinalProject
+//
+//  Created by Joshua Krueger on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
